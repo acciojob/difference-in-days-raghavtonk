55f984df7bc677632cb80fba3410afddf5688c71
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 20:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-};
+	let fulldate1 = new Date(date1);
+	let fulldate2 = new Date(date2);
 
+	let timediff = fulldate2 - fulldate1;
+
+	let daydiff = Math.floor(timediff/(1000*60*60*24));
+	
+	return daydiff; 
+};  
+ 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
+const dateTwo = prompt("Enter End Date."); 
 alert(dateDiffInDays(dateOne, dateTwo));
